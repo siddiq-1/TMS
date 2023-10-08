1ac48c80958ef73c17111dc43fa85e40b717caa8
siddi <siddi@DESKTOP-6MSF896>
2023-10-08 21:13:38 +0530

    Navigation Bar

---
+++ TMS.AngularClient/Client/src/app/all-task/all-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-task',
+  templateUrl: './all-task.component.html',
+  styleUrls: ['./all-task.component.css']
+})
+export class AllTaskComponent {
+
+}
